ff4b0b57b327ffb3def093b64a578e1226a45c34
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-05-05 21:02:31 -0400

    worked on App.js get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>MERN | Message Board</h1>
+class App extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
 
-      <div className="main">
-        <h3>Search for a message board</h3> <br />
-        <form>
-          <input className="input" placeholder="/b/general"/>
-          <button className="input" style={{width: "5%"}}>Submit</button>
-        </form>
-        <br /><br />
-        <a href="board.html" >Board</a>
-        <br /><br />
-        <a href="thread.html" >Thread</a>
+  // TODO: get input to redirect to /b/general/
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/b/general/');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/b/general/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>MERN | Message Board</h1>
+
+        <div className="main">
+          <h3>Search for a message board</h3> <br />
+          <form>
+            <input
+              className="input"
+              placeholder="/b/general"
+              value={this.state.post}
+              onChange={e => this.setState({ post: e.target.value })}
+              />
+            <button className="button" type="submit">Submit</button>
+          </form>
+          <br /><br />
+          <a href="board.html" >Board</a>
+          <br /><br />
+          <a href="thread.html" >Thread</a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
