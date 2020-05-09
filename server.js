cc13692c1b24832e016f4bc91acac77a138cdd4c
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-05-09 13:00:59 -0400

    added heroku for deployment

--- server.js
+++ server.js
@@ -67,4 +67,15 @@ app.listen(process.env.PORT || 5000, function () {
   }
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
 module.exports = app; //for testing
