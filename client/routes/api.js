64faeb560128e6260cab7d831d53e8a33216a943
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-06-10 13:43:37 -0400

    removed dotenv file and set mongodb string with heroku cli

--- client/routes/api.js
+++ client/routes/api.js
@@ -16,7 +16,7 @@ require('dotenv').config({ path: '.env' });
 let thread = require("../models/thread.js").thread;
 let reply = require("../models/reply.js").reply;
 
-const CONNECTION_STRING = ''; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.MONGO_DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true });
 
