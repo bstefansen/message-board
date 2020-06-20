04eefd71eeac7ab68e6ad859ddb04ac760707596
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-06-20 15:15:53 -0400

    deleted procfile and revamped server scripts

--- server.js
+++ server.js
@@ -77,11 +77,11 @@ app.listen(process.env.PORT || 5000, function () {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile('client/build', 'index.html');
   });
 }
 
